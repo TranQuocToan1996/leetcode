6b483ce89939a277c4242149482005e8bc05cb0c
ToanTran <tranquoctoan.ce@gmail.com>
2022-04-10 21:43:51 +0700

    10-04-2022

---
+++ easy/682. Baseball Game.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	tests := [][]string{
+		{"5", "2", "C", "D", "+"},
+		{"5", "-2", "4", "C", "D", "9", "+", "+"},
+		{"1"},
+	}
+	for _, test := range tests {
+		fmt.Println(calPoints(test))
+	}
+}
+
+// https://leetcode.com/problems/baseball-game/
+
+/*
+time complexity: O(n), 4 ms
+Space complexity: O(n), 2.6 MB
+*/
+
+func calPoints(ops []string) (sum int) {
+	stack := make([]int, len(ops))
+	top := 0
+	for _, op := range ops {
+		switch op {
+		case "D":
+			stack[top] = 2 * stack[top-1]
+			sum += stack[top]
+			top++
+		case "+":
+			stack[top] = stack[top-1] + stack[top-2]
+			sum += stack[top]
+			top++
+		case "C":
+			sum -= stack[top-1]
+			top--
+		default:
+			num, _ := strconv.Atoi(op)
+			stack[top] = num
+			sum += stack[top]
+			top++
+		}
+	}
+	return sum
+}
---
+++ medium/2. Add Two Numbers.go
@@ -0,0 +1,39 @@
+package main
+
+// https://leetcode.com/problems/add-two-numbers/
+
+/*
+time complexity: O(),  ms
+Space complexity: O(),  MB
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	carryNum := 0
+	l3 := new(ListNode)
+	curent := l3
+
+	for l1 != nil || l2 != nil || carryNum != 0 {
+		val1, val2 := 0, 0
+		if l1 != nil {
+			val1 = l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			val2 = l2.Val
+			l2 = l2.Next
+		}
+
+		val := val1 + val2 + carryNum
+		carryNum = val / 10
+		curent.Next = &ListNode{Val: val % 10, Next: nil}
+		curent = curent.Next
+	}
+
+	return l3.Next
+}
