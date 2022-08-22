54f6aac348c07a731609992acba33f6d92023db7
ToanTran <tranquoctoan.ce@gmail.com>
2022-08-22 21:49:15 +0700

    refactor to test for debug

--- easy/1528. Shuffle String.go
+++
@@ -1,1 +0,0 @@
-func restoreString(s string, indices []int) string {    newS := make([]byte, len(indices))    for i,v:=range indices{        newS[v] = s[i]    }    return string(newS)}// Thoi gian : n do phai duyet qua string// Khong gian: n do phai tao slice byte
