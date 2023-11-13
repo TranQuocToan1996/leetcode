1af4f4ccd46fdb88a5a9cb013e3cb74c630430b9
TranQuocToan1996 <tranquoctoan.ce@gmail.com>
2023-11-13 16:05:35 +0700

    55. Jump Game

---
+++ medium/55. Jump Game_test.go
@@ -0,0 +1,51 @@
+package medium
+
+import (
+	"testing"
+)
+
+func canJump(nums []int) bool {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if i > max {
+			return false
+		}
+		if max < i+nums[i] {
+			max = i + nums[i]
+		}
+	}
+	return true
+}
+
+func TestCanJump(t *testing.T) {
+	for _, test := range []struct {
+		input  []int
+		output bool
+	}{
+		{
+			input:  []int{2, 3, 1, 1, 4},
+			output: true,
+		},
+		{
+			input:  []int{3, 2, 1, 0, 4},
+			output: false,
+		},
+		{
+			input:  []int{3, 2, 3, 0, 4},
+			output: true,
+		},
+		{
+			input:  []int{0},
+			output: true,
+		},
+		{
+			input:  []int{2, 0, 0},
+			output: true,
+		},
+	} {
+		res := canJump(test.input)
+		if res != test.output {
+			t.Errorf("expect %v but got %v, test %v", test.output, res, test.input)
+		}
+	}
+}
