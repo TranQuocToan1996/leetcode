e23b3c950fafc9df120cffd90510360d53f7aa82
TranQuocToan1996 <tranquoctoan.ce@gmail.com>
2023-11-17 12:38:01 +0700

    78. Subsets

--- medium/78. Subsets_test.go
+++ medium/78. Subsets_test.go
@@ -7,7 +7,50 @@ import (
 	"leetcode/uti"
 )
 
+// Time: O(n*2^n)
+// Space: O(n*2^n)
+// func subsets(nums []int) [][]int {
+// 	n := len(nums)
+// 	queue := make([][]int, 0, int(math.Pow(2, float64(n))))
+// 	queue = append(queue, []int{})
+
+// 	for i := 0; i < n; i++ {
+// 		length := len(queue)
+// 		for j := 0; j < length; j++ {
+// 			temp := uti.DeepCopySlice(queue[j])
+// 			temp = append(temp, nums[i])
+// 			queue = append(queue, temp)
+// 		}
+// 	}
+
+// 	return queue
+// }
+
 func subsets(nums []int) [][]int {
+	n := len(nums)
+	results := make([][]int, 0, int(math.Pow(2, float64(n))))
+	subsetLength := 0
+	var backtrack func(int, []int)
+	backtrack = func(first int, cur []int) {
+		if len(cur) == subsetLength {
+			results = append(results, uti.DeepCopySlice(cur))
+			return
+		}
+		for i := first; i < n; i++ {
+			cur = append(cur, nums[i])
+			backtrack(i+1, cur)
+			cur = cur[:len(cur)-1]
+		}
+	}
+	for ; subsetLength < n+1; subsetLength++ {
+		backtrack(0, []int{})
+	}
+	return results
+}
+
+// Time: O(2^n).
+// Space: O(2^n)
+/* func subsets(nums []int) [][]int {
 	n := len(nums)
 	results := make([][]int, 0, int(math.Pow(2, float64(n))))
 	results = append(results, []int{})
@@ -28,7 +71,6 @@ func subsets(nums []int) [][]int {
 			return
 		}
 		for _, num := range nums {
-			// TODO: Check duplicate nums
 			for _, val := range s {
 				if num == val {
 					return
@@ -40,7 +82,7 @@ func subsets(nums []int) [][]int {
 	}
 	generate([]int{})
 	return results
-}
+} */
 
 func TestSubsets(t *testing.T) {
 	for _, test := range []struct {
@@ -70,7 +112,7 @@ func TestSubsets(t *testing.T) {
 	} {
 		match := 0
 		output := subsets(test.input)
-		// O(n^2). But n in the tests quite small, so accept this time.
+		// O(n^2). But n in the tests quite small.
 		for _, subsetOutput := range test.output {
 			for _, subset := range output {
 				if uti.UnorderMatch(subset, subsetOutput) {
