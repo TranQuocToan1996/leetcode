81a1664c5db4c8a7d3f9fc2925b976de2fa242af
TranQuocToan1996 <tranquoctoan.ce@gmail.com>
2023-12-01 16:12:28 +0700

    215. Kth Largest Element in an Array

--- medium/215. Kth Largest Element in an Array_test.go
+++ medium/215. Kth Largest Element in an Array_test.go
@@ -1,22 +1,55 @@
 package medium
 
 import (
-	"container/heap"
+	"math/rand"
 	"testing"
-
-	"leetcode/model"
 )
 
+// Quick select:
+// Time: Average O(n). Worst O(n^2)
+// Space: O(1)
 func findKthLargest(nums []int, k int) int {
-	h := model.IntMinHeap{}
-	for _, num := range nums {
-		heap.Push(&h, num)
-		for h.Len() > k {
-			heap.Pop(&h)
+	left, right := 0, len(nums)-1
+	for {
+		pivotIndex := left + rand.Intn(right-left+1)
+		newPivotIndex := partitionFindKthLargest(nums, left, right, pivotIndex)
+		if newPivotIndex == len(nums)-k {
+			return nums[newPivotIndex]
+		} else if newPivotIndex > len(nums)-k {
+			right = newPivotIndex - 1
+		} else {
+			left = newPivotIndex + 1
 		}
 	}
-	return h[0]
 }
+
+func partitionFindKthLargest(nums []int, left int, right int, pivotIndex int) int {
+	pivot := nums[pivotIndex]
+	nums[pivotIndex], nums[right] = nums[right], nums[pivotIndex]
+	storedIndex := left
+	for i := left; i < right; i++ {
+		if nums[i] < pivot {
+			nums[i], nums[storedIndex] = nums[storedIndex], nums[i]
+			storedIndex++
+		}
+	}
+	nums[right], nums[storedIndex] = nums[storedIndex], nums[right]
+	return storedIndex
+}
+
+// Method: MinHeap
+// Time: O(nlogk)
+// Space: O(k)
+// func findKthLargest(nums []int, k int) int {
+// 	h := model.IntMinHeap{}
+// 	for _, num := range nums {
+// 		heap.Push(&h, num)
+// 		for h.Len() > k {
+// 			heap.Pop(&h)
+// 		}
+// 	}
+// 	return h[0]
+// }
 
 func TestFindKthLargest(t *testing.T) {
 nextTest:
