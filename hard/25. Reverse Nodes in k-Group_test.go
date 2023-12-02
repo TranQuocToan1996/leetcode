9ba420e1f0481bccf4b972c8193af6f3506bd99c
TranQuocToan1996 <tranquoctoan.ce@gmail.com>
2023-12-02 15:23:48 +0700

    25. Reverse Nodes in k-Group

---
+++ hard/25. Reverse Nodes in k-Group_test.go
@@ -0,0 +1,106 @@
+package hard
+
+import (
+	"reflect"
+	"testing"
+
+	"leetcode/model"
+)
+
+func reverseKGroup(head *model.ListNode, k int) *model.ListNode {
+	node, count := head, 0
+	for count < k {
+		if node == nil {
+			return head
+		}
+		node = node.Next
+		count++
+	}
+
+	privHead := reverseKGroup(node, k)
+	for count > 0 {
+		//
+	}
+
+	return privHead
+}
+
+func TestReverseKGroup(t *testing.T) {
+	for _, test := range []struct {
+		head   *model.ListNode
+		k      int
+		output *model.ListNode
+	}{
+		{
+			head: &model.ListNode{
+				Val: 1,
+				Next: &model.ListNode{
+					Val: 2,
+					Next: &model.ListNode{
+						Val: 3,
+						Next: &model.ListNode{
+							Val: 4,
+							Next: &model.ListNode{
+								Val: 5,
+							},
+						},
+					},
+				},
+			},
+			k: 2,
+			output: &model.ListNode{
+				Val: 2,
+				Next: &model.ListNode{
+					Val: 1,
+					Next: &model.ListNode{
+						Val: 4,
+						Next: &model.ListNode{
+							Val: 3,
+							Next: &model.ListNode{
+								Val: 5,
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			head: &model.ListNode{
+				Val: 1,
+				Next: &model.ListNode{
+					Val: 2,
+					Next: &model.ListNode{
+						Val: 3,
+						Next: &model.ListNode{
+							Val: 4,
+							Next: &model.ListNode{
+								Val: 5,
+							},
+						},
+					},
+				},
+			},
+			k: 3,
+			output: &model.ListNode{
+				Val: 3,
+				Next: &model.ListNode{
+					Val: 2,
+					Next: &model.ListNode{
+						Val: 1,
+						Next: &model.ListNode{
+							Val: 4,
+							Next: &model.ListNode{
+								Val: 5,
+							},
+						},
+					},
+				},
+			},
+		},
+	} {
+		res := reverseKGroup(test.head, test.k)
+		if !reflect.DeepEqual(res, test.output) {
+			t.Errorf("expect %v but got %v, test %v", test.output, res, test)
+		}
+	}
+}
